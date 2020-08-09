6921e0585a631d5578e45903aa0ce7c4cb090c8b
Amir HS <amirhossein.zarezadeh@gmail.com>
2020-08-09 14:33:35 +0430

    static_root directory updated dh user

--- antiplagiarism/settings.py
+++ antiplagiarism/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('/home/amirhosseinz/', 'static/')
+STATIC_ROOT = os.path.join('/home/dh_mwbawy/', 'static/')
 
 # Email configurations
 EMAIL_HOST_PASSWORD = 'Iran_bazrpash1399'
