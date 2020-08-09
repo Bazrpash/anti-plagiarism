e2738b29d4165896a65dc8eae80bf3068251cfc9
Amir HS <amirhossein.zarezadeh@gmail.com>
2020-08-09 22:08:05 +0430

    celery broker configs added to settings

--- antiplagiarism/settings.py
+++ antiplagiarism/settings.py
@@ -134,9 +134,17 @@ EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 CELERY_EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 
-# EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
-# CELERY_EMAIL_TASK_CONFIG = {
-#     'max_retries': 30,
-#     'queue': 'mail_queue',
-#     'ignore_result': True,
-# }
+# Celery configurations
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Tehran'
+
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
+CELERY_EMAIL_TASK_CONFIG = {
+    'max_retries': 30,
+    'queue': 'mail_queue',
+    'ignore_result': True,
+}
